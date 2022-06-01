74088ebfea6bb37099d9a7368edd47eb51dfd419
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-01 18:48:50 +0200

    Update package .....entities.configurators

--- source/models/administrations/entities/configurators/definition/groups/tables/package.d
+++
@@ -1,9 +0,0 @@
-module models.administrations.entities.configurators.definition.groups.tables;
-
-@safe:
-import models.administrations;
-
-public import models.administrations.entities.configurators.definition.groups.tables.table;
-
-public import models.administrations.entities.configurators.definition.groups.tables.relation;
-
