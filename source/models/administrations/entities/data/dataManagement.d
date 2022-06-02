542fdce475c2a15fa77cf21c2e523ed0718a9d41
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-02 09:09:08 +0200

    Update package ....entities.datamanagements

--- source/models/administrations/entities/data/dataManagement.d
+++
@@ -1,2 +0,0 @@
-module models.administrations.entities.data.data;
-
---
+++ source/models/administrations/entities/datamanagements/package.d
@@ -0,0 +1,15 @@
+module models.administrations.entities.data;
+
+@safe:
+import models.administrations;
+
+public import models.administrations.entities.datamanagements.data;
+public import models.administrations.entities.datamanagements.definitionGroup;
+public import models.administrations.entities.datamanagements.definitionGroupDetail;
+public import models.administrations.entities.datamanagements.executionJob;
+public import models.administrations.entities.datamanagements.executionJobDetail;
+public import models.administrations.entities.datamanagements.targetMapEntity;
+public import models.administrations.entities.datamanagements.template_;
+public import models.administrations.entities.datamanagements.templateDetail;
+public import models.administrations.entities.datamanagements.templateSource;
+public import models.administrations.entities.datamanagements.templateSourceDependency;
