11edf65d163d1b0adf1a5b37cdea679a782ccca9
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-02 09:44:33 +0200

    Adding entities in dmf.definitiongroups

--- source/models/administrations/entities/dmf/package.d
+++ source/models/administrations/entities/dmf/package.d
@@ -3,6 +3,7 @@ module models.administrations.entities.dmf;
 @safe:
 import models.administrations;
 
-public import models.administrations.entities.dmf.definitionGroupTemplateHeader;
-public import models.administrations.entities.dmf.definitionGroupTemplateLine;
-public import models.administrations.entities.dmf.dmfEntityExecutionParameters;
+// Package
+public import models.administrations.entities.dmf.definitiongroups;
+
+public import models.administrations.entities.dmf.entityexecutionparameters;
