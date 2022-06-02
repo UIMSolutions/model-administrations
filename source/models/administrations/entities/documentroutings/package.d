c24e16853d8765d9e45a08f1df87831ebc028fb8
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-02 09:57:38 +0200

    Adding entities in ....entities.documentroutings

--- source/models/administrations/entities/documentroutings/package.d
+++ source/models/administrations/entities/documentroutings/package.d
@@ -1,2 +1,6 @@
-module models.administrations.entities.document_routing;
+module models.administrations.entities.documentroutings;
+
+public import models.administrations.entities.documentroutings.clientapp;
+public import models.administrations.entities.documentroutings.job;
+public import models.administrations.entities.documentroutings.printer;
 
--- source/models/administrations/entities/documentroutings/printer.d
+++ source/models/administrations/entities/documentroutings/printer.d
@@ -1,2 +1,38 @@
 module models.administrations.entities.documentroutings.printer;
 
+@safe:
+import models.administrations;
+
+class DDocumentRoutingPrinterEntity : DOOPEntity {
+  mixin(EntityThis!("DocumentRoutingPrinterEntity"));
+
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+        "clientApp": StringAttribute, //
+        "printer": StringAttribute, //
+        "printerName": StringAttribute, //
+        "printerDescription": StringAttribute, //
+        "printerPath": StringAttribute, //
+        "printerIsActive": StringAttribute, //
+        "printerId": StringAttribute, //
+        "printerCompanyId": StringAttribute, //
+        "clientApplicationId": StringAttribute, //
+        "clientApplicationDescription": StringAttribute, //
+        "backingTable_DocumentRoutingPrinterAppAssociationRelationshipId": StringAttribute, //
+      ])
+      .registerPath("administration_documentroutings.printers");
+  }
+}
+mixin(EntityCalls!("DocumentRoutingPrinterEntity"));
+
+version(test_model_administrations) {
+  unittest {
+    assert(DocumentRoutingPrinterEntity);
+
+    auto entity = DocumentRoutingPrinterEntity;
+    // TODO more tests
+  }
+}
