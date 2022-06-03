62046c0fb7e1f6ae67cc509f0e328dfeb5ea5bc6
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-03 07:23:59 +0200

    Adding comments

--- source/models/administrations/entities/configurators/definitions/package.d
+++ source/models/administrations/entities/configurators/definitions/package.d
@@ -3,4 +3,5 @@ module models.administrations.entities.configurators.definition;
 @safe:
 import models.administrations;
 
+// Subpackage
 public import models.administrations.entities.configurators.definition.groups;
--- source/models/administrations/entities/configurators/package.d
+++ source/models/administrations/entities/configurators/package.d
@@ -3,5 +3,6 @@ module models.administrations.entities.configurators;
 @safe:
 import models.administrations;
 
+// SUbpackages
 public import models.administrations.entities.configurators.definitions;
 public import models.administrations.entities.configurators.layouts;
